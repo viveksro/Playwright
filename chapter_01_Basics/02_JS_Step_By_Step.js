6852ff4c2eca0aefa88a90134e324c4f5b7bd146
Pramod Dutta <pramoddutta_96@yahoo.com>
2026-02-12 08:05:29 +0530

    Initial commit: add project files and .gitignore

---
+++ chapter_01_Basics/02_JS_Step_By_Step.js
@@ -0,0 +1,11 @@
+let a = 10;
+console.log(a);
+
+for (let a = 0; a < 100000; a++) {
+    console.log(a);
+    print();
+}
+
+function print() {
+    console.log("Hello");
+}
---
+++ chapter_01_Basics/03_verify_setup.js
@@ -0,0 +1,11 @@
+console.log(process.platform);
+// MAC - DARWIN
+// WINDOWS - WIN32
+// LINUX - LINUX
+
+console.log(process.arch);
+// x64
+// arm64
+
+
+console.log("Node Version:", process.version);
